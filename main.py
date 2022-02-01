a85ed8a28bd9c2a0167331921364540a560a84d6
Dicont <d.ivanov2006@mail.ru>
2022-02-01 18:53:31 +0300

    2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, requests, sys, os
+import requests, sys, os
 
 # Создайте оконное приложение, отображающее карту по координатам и в масштабе, который задаётся программно.
 from PyQt5 import uic
@@ -8,7 +8,7 @@ from PyQt5.QtWidgets import QMainWindow, QApplication
 
 
 class MapParams(object):
-    def __init__(self,lon,lat,zoom):
+    def __init__(self, lon, lat, zoom):
         self.lat = lon  # Координаты центра карты на старте. Задал координаты университета
         self.lon = lat
         self.zoom = zoom  # Масштаб карты на старте. Изменяется от 1 до 19
@@ -23,7 +23,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("./main.ui", self)
-        self.mp = MapParams(61, 50,16)
+        self.mp = MapParams(61, 50, 16)
         self.load_map()
 
     def load_map(self):
@@ -44,19 +44,20 @@ class MainWindow(QMainWindow):
             file.write(response.content)
         pixmap = QPixmap(map_file)
         self.label.setPixmap(pixmap)
-    def keyPressEvent(self,event):
+
+    def keyPressEvent(self, event):
         if event.key() == Qt.Key_Left:
-            self.mp.lon-=0.001
+            self.mp.lon -= 0.001
             self.load_map()
 
         if event.key() == Qt.Key_Right:
-            self.mp.lon+=0.001
+            self.mp.lon += 0.001
             self.load_map()
         if event.key() == Qt.Key_Up:
-            self.mp.lat+=0.001
+            self.mp.lat += 0.001
             self.load_map()
         if event.key() == Qt.Key_Down:
-            self.mp.lat-=0.001
+            self.mp.lat -= 0.001
             self.load_map()
         if event.key() == Qt.Key_PageUp:
             if self.mp.zoom <= 20:
@@ -66,9 +67,6 @@ class MainWindow(QMainWindow):
             if self.mp.zoom >= 5:
                 self.mp.zoom -= 1
                 self.load_map()
-
-
-
 
 
 def except_hook(cls, exception, traceback):
