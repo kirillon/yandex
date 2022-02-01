dbf17edb9568417ae6e39a98be82bcfa3bf116dc
kirillon <kirilminecraft@gmail.com>
2022-02-01 18:53:05 +0300

    dg

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import pygame, requests, sys, os
 
 # Создайте оконное приложение, отображающее карту по координатам и в масштабе, который задаётся программно.
 from PyQt5 import uic
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5.QtCore import Qt
 
 
 class MapParams(object):
-    def __init__(self, zoom):
-        self.lat = 61.665279  # Координаты центра карты на старте. Задал координаты университета
-        self.lon = 50.813492
+    def __init__(self,lon,lat,zoom):
+        self.lat = lon  # Координаты центра карты на старте. Задал координаты университета
+        self.lon = lat
         self.zoom = zoom  # Масштаб карты на старте. Изменяется от 1 до 19
         self.type = "map"  # Другие значения "sat", "sat,skl"
 
@@ -23,13 +23,13 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("./main.ui", self)
-        self.zoom = 16
+        self.mp = MapParams(61, 50,16)
         self.load_map()
 
     def load_map(self):
-        mp = MapParams(self.zoom)
-        map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=mp.ll(), z=mp.zoom,
-                                                                                        type=mp.type)
+
+        map_request = "http://static-maps.yandex.ru/1.x/?ll={ll}&z={z}&l={type}".format(ll=self.mp.ll(), z=self.mp.zoom,
+                                                                                        type=self.mp.type)
         response = requests.get(map_request)
         if not response:
             print("Ошибка выполнения запроса:")
@@ -44,14 +44,31 @@ class MainWindow(QMainWindow):
             file.write(response.content)
         pixmap = QPixmap(map_file)
         self.label.setPixmap(pixmap)
+    def keyPressEvent(self,event):
+        if event.key() == Qt.Key_Left:
+            self.mp.lon-=0.001
+            self.load_map()
 
-    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Right:
+            self.mp.lon+=0.001
+            self.load_map()
+        if event.key() == Qt.Key_Up:
+            self.mp.lat+=0.001
+            self.load_map()
+        if event.key() == Qt.Key_Down:
+            self.mp.lat-=0.001
+            self.load_map()
         if event.key() == Qt.Key_PageUp:
-            if self.zoom <= 20:
-                self.zoom += 1
+            if self.mp.zoom <= 20:
+                self.mp.zoom += 1
+                self.load_map()
         if event.key() == Qt.Key_PageDown:
-            if self.zoom >= 5:
-                self.zoom -= 1
+            if self.mp.zoom >= 5:
+                self.mp.zoom -= 1
+                self.load_map()
+
+
+
 
 
 def except_hook(cls, exception, traceback):
